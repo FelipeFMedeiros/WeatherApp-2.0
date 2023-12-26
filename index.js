5e14542c93a97fdaa90e5db91dbf336bff0129cd
FelipeFMedeiros <felipeff_freitas@outlook.com>
2023-12-25 22:01:22 -0300

    Fix some bugs

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
-
-sideLinks.forEach(item => {
-    const li = item.parentElement;
-    item.addEventListener('click', () => {
-        sideLinks.forEach(i => {
-            i.parentElement.classList.remove('active');
-        })
-        li.classList.add('active');
-    })
-});
-
 const menuBar = document.querySelector('.content nav .bx.bx-menu');
 const sideBar = document.querySelector('.sidebar');
 
