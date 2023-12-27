31983104c89a9714b33c61fc443077f369ad7488
FelipeFMedeiros <felipeff_freitas@outlook.com>
2023-12-27 00:27:10 -0300

    Try fix SideBar Bug #5

--- src/SidebarContext.jsx
+++ src/SidebarContext.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const SidebarContext = createContext();
 
 export const SidebarProvider = ({ children }) => {
-  const [isSidebarClosed, setIsSidebarClosed] = useState(false);
+  const [isSidebarClosed, setIsSidebarClosed] = useState(true);
 
   const toggleSidebar = () => {
     setIsSidebarClosed((prev) => !prev);
