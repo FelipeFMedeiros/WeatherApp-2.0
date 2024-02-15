8cd1c10eede28b12c13a541e9d3f38beb482be28
FelipeFMedeiros <felipeff_freitas@outlook.com>
2024-02-14 22:45:21 -0300

    Modified not found page
    Adiciona uma animação de fontSize ao btn:hover e adiciona a página notfound para as páginas de settings e info, pois não estão feitas ainda.

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from 'react';
+
 function Info() {
+  useEffect(() => {
+    window.location.href = '/notfound';
+  }, []);
+
   return (
     <div className="Info">
       <h1>Informações</h1>
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
+
 function Settings() {
+  useEffect(() => {
+    window.location.href = '/notfound';
+  }, []);
+
   return (
     <div className="Settings">
       <h1>Settings</h1>
     </div>
   );
 }
+
 export default Settings;
