421e5117f75024805a9f6faad8efdd45fd7eba9f
FelipeFMedeiros <felipeff_freitas@outlook.com>
2024-02-16 14:18:01 -0300

    Modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { server as localServer } from './vite.config.local.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: localServer
 })
